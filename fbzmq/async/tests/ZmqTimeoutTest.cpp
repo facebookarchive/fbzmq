0fdec388c33039e047cb98d2c00b73e4882db23e
Saif Hasan <has@fb.com>
2019-11-22 15:09:10 -0800

    Add ZmqTimeout test with folly::EventBase
    Summary: As titled.
    
    Reviewed By: jstrizich
    
    Differential Revision: D18643825
    
    fbshipit-source-id: 4e46560a85802e7c68af87c82eee3c9e12d9b051

--- fbzmq/async/tests/ZmqTimeoutTest.cpp
+++ fbzmq/async/tests/ZmqTimeoutTest.cpp
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include <folly/io/async/EventBase.h>
 #include <folly/synchronization/Baton.h>
 #include <gflags/gflags.h>
 #include <gtest/gtest.h>
@@ -16,7 +17,7 @@ namespace fbzmq {
 
 using namespace std::chrono_literals;
 
-TEST(ZmqTimeoutTest, TimeoutTest) {
+TEST(ZmqTimeoutTest, ZmqEventLoop) {
   fbzmq::ZmqEventLoop evl;
 
   auto start = std::chrono::steady_clock::now();
@@ -64,6 +65,52 @@ TEST(ZmqTimeoutTest, TimeoutTest) {
   // Cleanup
   evl.stop();
   evlThread.join();
+}
+
+TEST(ZmqTimeoutTest, FollyEventBase) {
+  folly::EventBase evb;
+
+  auto start = std::chrono::steady_clock::now();
+  size_t count{0};
+  folly::Baton<> waitBaton;
+
+  // Schedule first timeout
+  auto timeout = ZmqTimeout::make(&evb, [&]() noexcept {
+    auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::steady_clock::now() - start);
+    LOG(INFO) << "Executing callback after " << diff.count() << "ms.";
+
+    // fetch_add returns previous value
+    ++count;
+    if (count >= 4) {
+      waitBaton.post();
+    }
+  });
+
+  EXPECT_NO_THROW(timeout->cancelTimeout());
+  timeout->scheduleTimeout(100ms, true /* periodic */);
+  EXPECT_TRUE(timeout->isScheduled());
+
+  // can schedule twice with no issue
+  timeout->scheduleTimeout(100ms, true /* periodic */);
+  EXPECT_TRUE(timeout->isScheduled());
+  EXPECT_TRUE(timeout->isPeriodic());
+
+  // Start event loop
+  std::thread evbThread([&]() {
+    LOG(INFO) << "Starting event-base thread.";
+    evb.loopForever();
+    LOG(INFO) << "Stopping event-base thread.";
+  });
+
+  // Wait for completion
+  waitBaton.wait();
+  timeout.reset();
+  EXPECT_EQ(4, count);
+
+  // Cleanup
+  evb.terminateLoopSoon();
+  evbThread.join();
 }
 
 } // namespace fbzmq
